ffeb769a84a97e98427e2fd43d64f9aedb4cf4e7
Jamshid <jamshu.mkd@gmail.com>
2020-11-18 06:54:59 +0530

    route

--- __manifest__.py
+++ __manifest__.py
@@ -3,15 +3,14 @@
     'name': "product_api",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+       Return product Details """,
 
     'description': """
-        Long description of module's purpose
+       Return product Details 
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Jamshi",
+    'website': "github.com/jamshu",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
@@ -25,7 +24,7 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
+       
         'views/templates.xml',
     ],
     # only loaded in demonstration mode
--- controllers/controllers.py
+++ controllers/controllers.py
@@ -5,35 +5,31 @@ from odoo import http
 
 
 
-    # @http.route(['/my/invoices/<int:invoice_id>'], type='http', auth="public", website=True)
-    # def portal_my_invoice_detail(self, invoice_id, access_token=None, report_type=None, download=False, **kw):
-    #     try:
-    #         invoice_sudo = self._document_check_access('account.move', invoice_id, access_token)
-    #     except (AccessError, MissingError):
-    #         return request.redirect('/my')
-
-    #     if report_type in ('html', 'pdf', 'text'):
-    #         return self._show_report(model=invoice_sudo, report_type=report_type, report_ref='account.account_invoices', download=download)
-
-    #     values = self._invoice_get_page_view_values(invoice_sudo, access_token, **kw)
-    #     acquirers = values.get('acquirers')
-    #     if acquirers:
-    #         country_id = values.get('partner_id') and values.get('partner_id')[0].country_id.id
-    #         values['acq_extra_fees'] = acquirers.get_acquirer_extra_fees(invoice_sudo.amount_residual, invoice_sudo.currency_id, country_id)
-
-    #     return request.render("account.portal_invoice_page", values)
-
-
 class ProductApi(http.Controller):
-    @http.route('/product_api/', auth='public')
+    @http.route('/api/product', auth='public')
     def index(self, **kw):
-        return "Hi"
+        return "Hi,Enter the product ID in the URL"
 
-    @http.route('/product_api/<int:product_id>', auth='public')
+    @http.route('/api/product/<int:product_id>', auth='public')
     def list(self,product_id, **kw):
         return http.request.render('product_api.listing', {
             'root': '/product_api',
             'objects': http.request.env['product.product'].search([('id','=',product_id)]),
         })
+    
+    @http.route('/api/product/json/<int:product_id>',type='json' auth='public')
+    def json_data(self,product_id, **kw):
+        data =[]
+        product_sudo = request.env['product.product'].sudo().browse(product_id)
+        if not product_sudo:
+            rerurn False
+        
+        data.append({'product':product_sudo.name,
+                    'price':product.list_price,
+                    'cost':product.standard_price,
+                    'OnHandQty':product.qty_available,
+                    'category':product.categ_id.name})
+
+        return data
 
    
